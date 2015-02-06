3765fd8d681605ca8ced8064207caff049ed323b
JoséArmando <JosArmando@10.180.71.93>
2015-02-06 16:08:50 +0000

    almost last changes

--- src/CATPayrollSystem/Employee.java
+++ src/CATPayrollSystem/Employee.java
@@ -5,10 +5,64 @@
  */
 package CATPayrollSystem;
 
+import java.util.List;
+
 /**
  *
  * @author eddychou
  */
 public class Employee {
+    
+    private String name;
+    private String position;
+    private Agreement agreement;
+    private List<Entry> accountingEntries; 
+
+    public Employee(String name, String position, Agreement agreement) {
+        this.name = name;
+        this.position = position;
+        this.agreement = agreement;
+    }
+
+    public Employee(String name, Agreement agreement) {
+        this.name = name;
+        this.agreement = agreement;
+    }
+
+    public Agreement getAgreement() {
+        return agreement;
+    }
+
+    public void setAgreement(Agreement agreement) {
+        this.agreement = agreement;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public List<Entry> getAccountingEntries() {
+        return accountingEntries;
+    }
+
+    public void setAccountingEntries(List<Entry> accountingEntries) {
+        this.accountingEntries = accountingEntries;
+    }
+    
+    public void addAccountingEntries(Entry accountingEntry) {
+        accountingEntries.add(accountingEntry);
+    }
     
 }
---
+++ src/View/CATPayroll.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+/**
+ *
+ * @author JoséArmando, Casey and Azry
+ */
+public class CATPayroll {
+    
+    private void consoleUI(){
+        System.out.println("Hi, OH MEN THAT FADI");
+    }
+    
+    public static void main (String[] args){
+        new CATPayroll().consoleUI();
+        
+    }
+}
